158b77db206fa2c8d5d35d211080044b03f9bd45
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2024-10-21 16:25:20 +0530

    add route config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import { Button } from './components/ui/button'
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/auth";
 
 function App() {
   return (
     <>
-      <Button className="bg-red-600">Check</Button>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
